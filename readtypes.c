0e879c479027ae597ce7f576e1930ea248d1df60
Alex Pogue <alex@epogue.com>
2015-08-30 03:16:44 -0500

    Abstract chompline into new function

--- readtypes.c
+++ readtypes.c
@@ -7,14 +7,19 @@ int rt_string(char* str, int size, FILE* stream) {
         return 0;
     }
     size_t len = strlen(str);
-    if(str[len-1] != '\n') {
-        char ch;
-        while((ch = getchar()) != '\n') {
-            if(ch == EOF) break;
-        }
-    }
-    else {
+    if(str[len-1] != '\n')
+        rt_chompline(stream);
+    else
         str[len-1] = '\0';
-    }
+    return 1;
+}
+
+int rt_int(int* out, FILE* stream) {
+}
+
+int rt_chompline(FILE* stream) {
+    int ch;
+    while((ch = getchar()) != '\n')
+        if(ch == EOF) return 0;
     return 1;
 }
